2e05e60a31a5581b67546922a7e899fa94bb668d
Bogdan <opreatudorbogdan@gmail.com>
2025-01-16 15:55:34 +0200

    -added selenium, junit, log4j and demo test

---
+++ src/main/java/org/automation/framework/BrowserManager.java
@@ -0,0 +1,17 @@
+package org.automation.framework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserManager {
+
+    WebDriver driver;
+
+    public void openBrowser() {
+        driver = new ChromeDriver();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
